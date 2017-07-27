30384d40acba63418acf4365c91907e771f9eae8
Muhammad Devin Pradipta <devindipta@gmail.com>
2017-07-27 20:32:20 +0700

    tambah splash screen,update halaman awal (Profil)

--- app/src/main/java/com/example/devinpradipta/as_sunnah/aturProgress.java
+++ app/src/main/java/com/example/devinpradipta/as_sunnah/aturProgress.java
@@ -1,6 +1,7 @@
 package com.example.devinpradipta.as_sunnah;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,11 +10,32 @@ import android.widget.Button;
 public class aturProgress extends AppCompatActivity {
     Button masuk;
     Intent masuks;
+    private static int SPLASH_TIME_OUT = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_atur_progress);
 
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(getApplicationContext(), HalamanProfil.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
-}
+    }
+
